b9ca7ff5c40ca5fe0cb86d8fa7b4bd93004d1c9a
lima08 <joaopaulo.gomeslima8@gmail.com>
2022-01-23 14:32:47 -0300

    Adiciona exercicio 1 soluçao - feita com apoio.

--- computer-science/bl32-intro_python/32.2/exercises/solutions/ex2.py
+++ computer-science/bl32-intro_python/32.2/exercises/solutions/ex2.py
@@ -11,5 +11,46 @@
 
 # O sorteio de uma palavra aleatória pode ser feito utilizando o método choice:
 #  random.choice(["word1", "word2", "word3"]) -> "word2".
-with open('exercises/helper/words_list.txt', 'r') as words:
-    
+
+import random
+
+WORDS = [
+    "cat",
+    "elephant",
+    "dog",
+    "monkey",
+    "duck",
+    "chameleon",
+    "bear",
+    "moose",
+    "rooster",
+]
+MAX_ATTEMPTS = 3
+
+
+def draw_secret_word(words):
+    secret_word = random.choice(words)
+    scrambled_word = ''.join(random.sample(secret_word, len(secret_word)))
+    return secret_word, scrambled_word
+
+
+def collect_guesses():
+    guesses = []
+    for attempt in range(MAX_ATTEMPTS):
+        guess = input('Make a attempt: ')
+        guesses.append(guess)
+    return guesses
+
+
+def check_game_result(secret_word, guesses):
+    if secret_word in guesses:
+        print(f'You win! The word is {secret_word}.')
+    else:
+        print(f'You lose! The word is {secret_word}.')
+
+
+if __name__ == "__main__":
+    secret_word, scrambled_word = draw_secret_word(WORDS)
+    print(f'The secret word is {scrambled_word}.')
+    guesses = collect_guesses()
+    check_game_result(secret_word, guesses)
