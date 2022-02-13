c658a0f91f3c2c3f19e7c52c1b2e38b74204718d
lima08 <joaopaulo.gomeslima8@gmail.com>
2022-02-13 12:09:40 -0300

    Adiciona bl34.

---
+++ computer-science/bl34-redes_e_raspagem-de-dados/34.3/content/content.py
@@ -0,0 +1,22 @@
+#  Cheatsheet para css e xpath
+# https://devhints.io/css
+# https://devhints.io/xpath
+
+# from urllib import response
+# from parsel import Selector
+
+
+# text = (
+#  "<html><body><h1>Hello, Parsel!</h1><h1>Hello, Parsel2!</h1></body></html>"
+# )
+# # print(text)
+
+# selector = Selector(text=text)
+# print(selector.css('h1').getall())
+# print(selector.xpath('//h1').getall())
+# print(selector.css('h1::text').getall())
+# print(selector.xpath('//h1/text()').getall())
+
+# resp = requests.get('https://www.betrybe.com/')
+# print(resp.status_code)
+# print(resp.cookies)
